e4d87f5dff7675f189efce14b5bafbaa7538ac16
Adam Shwartz <adam@shwartz.co>
2018-08-15 19:50:00 +0300

    Full project before css

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ const app = express();
 app.use(express.static('./dist/book-lib'));
 
 app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname,'/dist/book-lib/index.html'));
+  res.sendFile(path.join(__dirname,'./dist/book-lib/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
